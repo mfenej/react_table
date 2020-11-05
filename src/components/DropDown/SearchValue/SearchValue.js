4cd4ac68a543a9d8eeed7c1af227dceee7738e35
Omar Al-Ayoubi <49438943+OmarAl-Ayoubi@users.noreply.github.com>
2020-11-05 13:33:43 +0200

    added infinit loader for search by value in dropDown

--- src/components/DropDown/SearchValue/SearchValue.js
+++ src/components/DropDown/SearchValue/SearchValue.js
@@ -1,14 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classes from './SearchValue.module.css';
 import CheckBox from '../CheckBox/CheckBox'
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions/index';
+import InfiniteScroll from "react-infinite-scroll-component";
 
 
 
 
 let arrBool = [];
+let arrData = [];
+
 const SearchValue = props => {
+    let [haveMore, setHaveMore] = useState(true);
+    let [itemCount, setitemCount] = useState(20);
+
+
     let [allChecked, setALLChecked] = useState(true);
     useEffect(() => {
         arrBool = [];
@@ -25,14 +32,14 @@ const SearchValue = props => {
 
 
 
-    const checkBoolAllFalse = () => {
+    const checkBoolAllFalse = useCallback(() => {
         let bool = true;
         arrBool.map(el => {
             if (el === true)
                 bool = false;
         })
         return bool
-    }
+    })
 
     useEffect(() => {
         if (!checkBoolAllFalse() && !allChecked) {
@@ -48,7 +55,7 @@ const SearchValue = props => {
         }
     }, [allChecked, props.id])
 
-    const selectAll = () => {
+    const selectAll = useCallback(() => {
         setALLChecked(!allChecked);
 
         if (!allChecked) {
@@ -60,8 +67,8 @@ const SearchValue = props => {
                 arrBool[i] = false;
             })
         }
-    }
-    const singleSelect = (id, check) => {
+    });
+    const singleSelect = useCallback((id, check) => {
         let i = props.data.indexOf(id);
         arrBool[i] = check;
 
@@ -75,30 +82,46 @@ const SearchValue = props => {
                 setALLChecked(true);
             }, 2);
         }
+    });
+    arrData = props.data.slice(0, itemCount);
+    const fetchMoreData = () => {
+        if (props.data.length - itemCount >= 20)
+            setitemCount(itemCount + 20);
+        else {
+            setHaveMore(false);
+            setitemCount(itemCount + props.data.length - itemCount);
+        }
     }
-
-
     return <div style={{ width: '100%', height: '80%', backgroundColor: '#354f69' }}>
-        
-        <div className={classes.checkBoxCont} style={{ height: '11.2rem', overflowY: 'scroll' }}>
+
+        <div className={classes.checkBoxCont} style={{ height: '11.2rem' }}>
 
             <CheckBox text={'SelectAll'}
                 checked={allChecked}
                 handler={selectAll}
                 key={0}
             />
+            <InfiniteScroll
+                dataLength={arrData.length}
+                next={fetchMoreData}
+                hasMore={haveMore}
+                loader={<h4>Loading...</h4>}
+                height={95}
+                className={classes.checkBoxCont}
+            >
+                {arrData.map((el, i) => (
 
-            {props.data.map((el, i) => {
+                    <CheckBox
+                        text={el}
+                        checked={arrBool[i]}
+                        handler={singleSelect}
+                        key={i + 1}
+                    />
 
-                return <CheckBox
-                    text={el}
-                    checked={arrBool[i]}
-                    handler={singleSelect}
-                    key={i + 1}
-                />
+                ))
+                }
+            </InfiniteScroll>
 
-            })
-            }
         </div>
     </div>
 };
